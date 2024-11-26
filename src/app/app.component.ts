c4fa302c613af6f0c99155cc5f3f3520a2a71c90
wikki-devops <wikkizofficial@gmail.com>
2024-11-26 13:24:04 +0530

    2nd update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+declare var $: any;
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'karunaiprakash';
+  emailContact: string = 'mail@karunaiprakash.com';
+
+  constructor(private renderer: Renderer2) { }
+
+  faq: any[] = [
+    { question: 'Question 1', answer: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores nihil labore veniam, quo pariatur, blanditiis obcaecati ullam molestiae eius minima quis ratione assumenda a!' },
+    { question: 'Question 2', answer: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores nihil labore veniam, quo pariatur, blanditiis obcaecati ullam molestiae eius minima quis ratione assumenda a!' },
+    { question: 'Question 3', answer: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores nihil labore veniam, quo pariatur, blanditiis obcaecati ullam molestiae eius minima quis ratione assumenda a!' },
+    { question: 'Question 4', answer: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores nihil labore veniam, quo pariatur, blanditiis obcaecati ullam molestiae eius minima quis ratione assumenda a!' },
+    { question: 'Question 5', answer: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores nihil labore veniam, quo pariatur, blanditiis obcaecati ullam molestiae eius minima quis ratione assumenda a!' },
+    { question: 'Question 6', answer: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores nihil labore veniam, quo pariatur, blanditiis obcaecati ullam molestiae eius minima quis ratione assumenda a!' },
+  ];
+
+  ngAfterViewInit() {
+    const script = this.renderer.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'assets/vendor/bootstrap/js/bootstrap.bundle.min.js';
+    script.src = 'assets/vendor/php-email-form/validate.js';
+    script.src = 'assets/vendor/aos/aos.js';
+    script.src = 'assets/vendor/glightbox/js/glightbox.min.js';
+    script.src = 'assets/vendor/swiper/swiper-bundle.min.js';
+    script.src = 'assets/vendor/purecounter/purecounter_vanilla.js';
+    script.src = 'assets/js/main.js';
+
+    this.renderer.appendChild(document.body, script);
+
+  }
+
 }
